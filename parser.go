70405272a3c7b96f8b84110ff0e0d3b8ecfe7545
Rachid AMRI <ramri@zendesk.com>
2018-04-28 13:25:46 +0100

    Add function to extract comments from query

--- parser.go
+++ parser.go
@@ -160,5 +160,23 @@ func parseEntry(lines []string) LogEvent {
 	}
 
 	event["Statement"] = strings.TrimSpace(strings.Join(queryLines, "\n"))
+
 	return event
+}
+
+func between(value string, a string, b string) string {
+	// Get substring between two strings.
+	posFirst := strings.Index(value, a)
+	if posFirst == -1 {
+		return ""
+	}
+	posLast := strings.Index(value, b)
+	if posLast == -1 {
+		return ""
+	}
+	posFirstAdjusted := posFirst + len(a)
+	if posFirstAdjusted >= posLast {
+		return ""
+	}
+	return value[posFirstAdjusted:posLast]
 }
